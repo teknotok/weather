0e05df8b867aa5007216b2624a895666bb081c29
teknotok <45201799+teknotok@users.noreply.github.com>
2021-08-17 02:06:53 +0300

    almost finish

--- scripts/app.js
+++ scripts/app.js
@@ -1,12 +1,33 @@
 //start searching from customer
 const weatherForm = document.querySelector(".change-location");
-const tempDiv = document.querySelector(".temp");
+const card = document.querySelector(".card");
+const details = document.querySelector(".details");
+
+const updateUI = (data) => {
+  details.innerHTML = `
+    <h5 class="my-3"></h5>
+    <div class="my-3">${data.weather[0].WeatherText}</div>
+    <div class="display-4 my-4">
+    <span>temp</span>
+    <span class="temp">${data.temp}</span>
+    <span>&deg;C</span>
+    </div>
+    `;
+  if (card.classList.contains("d-none")) {
+    card.classList.remove("d-none");
+  }
+};
 
 const updateCity = async (city) => {
   const cityKey = await getCity(city);
   const weather = await getWeather(cityKey);
   console.log(weather);
-  return weather;
+  const temp = weather[0].Temperature.Metric.Value;
+  return {
+    cityKey,
+    weather,
+    temp,
+  };
 };
 
 weatherForm.addEventListener("submit", (e) => {
@@ -14,7 +35,7 @@ weatherForm.addEventListener("submit", (e) => {
   const cityName = weatherForm.city.value.trim();
   weatherForm.reset();
   updateCity(cityName).then((data) => {
-    //tempDiv.textContent = data;
-    console.log(data)
+    updateUI(data);
+    /* tempDiv.textContent = data; */
   });
 });
--- scripts/forecast.js
+++ scripts/forecast.js
@@ -5,18 +5,16 @@ const getCity = async (city) => {
   const base = "http://dataservice.accuweather.com/locations/v1/cities/search";
   const query = key + "&q=" + city;
 
-  const responce = await fetch(base + query,{mode: 'no-cors',cache: 'no-cache'});
+  const responce = await fetch(base + query);
   const dataCityKey = await responce.json();
 
   return dataCityKey[0].Key;
 };
-getCity('adana').then(data => {console.log(data)})
-
 //get weather information
 const getWeather = async (id) => {
   const base = "http://dataservice.accuweather.com/currentconditions/v1/";
   const query = id + key;
-  const responce = await fetch(base + query, { mode: 'no-cors'});
+  const responce = await fetch(base + query);
   const dataWeather = await responce.json();
   return dataWeather;
 };
